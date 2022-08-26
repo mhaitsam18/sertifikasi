bbe9d23a21f90d11030f40a7f9c90ad8ef525b95
mhaitsam18 <haitsam03@gmail.com>
2022-08-26 22:01:45 +0700

    Update sertifikasi.sql

--- public/db/sertifikasi.sql
+++ public/db/sertifikasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2022 at 06:30 PM
+-- Generation Time: Aug 25, 2022 at 03:09 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
